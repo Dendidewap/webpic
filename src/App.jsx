b762c1d35df42ac5a155ae3467b5b827bd7b2564
Dendidewap <queenrora29@gmail.com>
2025-05-29 14:08:17 +0700

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,61 @@
 import React from "react";
 import "./App.css";
+import Product from "./product.jsx";
 
 function App() {
   return (
-    <div className="container grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-3 xl:gap-4 my-10">
-      <h2 className="text-xl font-semibold text-slate-600 md:col-start-4 md:row-start-2 md:flex">
-        <span className="self-center md:ml-3 xl:ml-14 "> Welcome Den </span>
-      </h2>
-      <h2 className="text-lg font-semibold text-slate-600 md:col-start-2  mb-5 md:row-start-2 md:flex">
-        <span className="self-end xl:mb-1 xl:ml-3">@Denxdwp_</span>
-      </h2>
+    <>
+      <div className="mx-48 mb-20 ">
+        <div className="font-bold text-4xl flex justify-center text-slate-300 mt-10 dewa">
+          MY GALLERY
+        </div>
+      </div>
+      <div className="container grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-3 xl:gap-4 my-10">
+        <h2 className="text-xl font-semibold text-slate-600 md:col-start-4 md:row-start-2 md:flex">
+          <span className="self-center md:ml-3 xl:ml-14 "> Welcome Den </span>
+        </h2>
+        <h2 className="text-lg font-semibold text-slate-600 md:col-start-2  mb-5 md:row-start-2 md:flex">
+          <span className="self-end xl:mb-1 xl:ml-3">@Denxdwp_</span>
+        </h2>
 
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/1.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/1.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/2.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/3.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/4.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/5.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-2 overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/6.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-4 overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/7.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/8.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/9.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
+        <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
+          <div className="w-full h-full bg-[url('./img/10.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+        </div>
       </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/2.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
+      <div className="mt-36 ">
+        <div className="font-bold text-4xl flex justify-center text-slate-300 mt-10 mb-20 dewa">
+          MY PRODUCT
+        </div>
+        <Product />
       </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/3.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/4.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/5.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-2 overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/6.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-4 overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/7.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/8.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/9.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-      <div className="h-30 md:h-50 xl:h-45 bg-lime-500 aspect-[4/3] rounded-lg md:aspect-[3/4] xl:aspect-[4/3] md:col-start-3 overflow-hidden group relative hover:scale-95 transition-all duration-500">
-        <div className="w-full h-full bg-[url('./img/10.jpeg')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/product.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+const Product = () => {
+  const url = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([]);
+
+  const getDataProducts = async () => {
+    const response = await fetch(url);
+    const dataProduct = await response.json();
+    setProducts(dataProduct);
+  };
+
+  useEffect(() => {
+    getDataProducts();
+  });
+
+  return (
+    <div className="container  ">
+      <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-x-9 gap-y-0.5 ">
+        {products.map((produk) => {
+          return (
+            <CardProduct
+              key={produk.id}
+              title={produk.title}
+              description={produk.description}
+              price={produk.price}
+              image={produk.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const CardProduct = (props) => {
+  return (
+    <div
+      className="max-w-sm rounded overflow-hidden shadow-lg mt-15 bg-white w-60 h-110 "
+      key={props.id}
+    >
+      <img
+        className="w-[75%] ml-8 mt-10 h-50"
+        src={props.image}
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-lg mb-2">
+          {props.title.substring(0, 15)}...
+        </div>
+
+        <p className="text-gray-700  text-xs">USD {props.price}$</p>
+        <p className="text-gray-700  text-xs">
+          {props.description.substring(0, 95)}...
+        </p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mt-4">
+          Buy
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
